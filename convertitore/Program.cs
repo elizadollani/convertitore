e9e5f8430d30320721abb01bcad56f8c709675bc
ELIZA DOLLANI <elizadollani@gmail.com>
2020-11-03 10:29:01 +0100

    convertitore da decimale a binario

--- convertitore/Program.cs
+++ convertitore/Program.cs
@@ -6,7 +6,22 @@ namespace convertitore
 	{
 		static void Main(string[] args)
 		{
-			
+			int numero;
+			int resto;
+			string numeroConvertito = "";
+			int divisore;
+			Console.WriteLine("Inserisci un numero:");
+			numero = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Inserisci un divisore");
+			divisore = Convert.ToInt32(Console.ReadLine());
+			while (numero > 0)
+			{
+				resto = numero % divisore;
+				numero = numero / 2;
+				numeroConvertito = Convert.ToString (resto) + numeroConvertito;
+			}
+			Console.WriteLine($"{numeroConvertito}");
+			Console.ReadLine();
 		}
 	}
 }
